17b3297b57ee0aa5d0a238a05b7f5a432b441d05
Rafael Queiroz Santos <rafaelqsantos33@gmail.com>
2024-07-13 14:16:35 -0300

    windturbine rename

--- windturbine_productor.py
+++ windturbine_productor.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 from time import sleep
 from datetime import datetime
-from random import uniform,randint
+from random import uniform
 
 
 def gerar_registro() -> dict[str,str]:
@@ -19,25 +19,25 @@ def gerar_registro() -> dict[str,str]:
 
 temperature_id = 1
 while True:
-    # Gera uma lista com todos os windmills
-    windmills_list = ['json_windmill','csv_windmill','parquet_windmill']
+    # Gera uma lista com todos os windturbines
+    windturbines_list = ['json_windturbine','csv_windturbine','parquet_windturbine']
     # Gera o registro
     register = gerar_registro() 
     
     count = temperature_id
-    for current_windmill_name in windmills_list:
+    for current_windturbine_name in windturbines_list:
 
         register['idtemp'] = str(count)
-        register['name'] = current_windmill_name
+        register['name'] = current_windturbine_name
         
-        match current_windmill_name:
-            case 'json_windmill':
+        match current_windturbine_name:
+            case 'json_windturbine':
                 with open('data/data.json','w') as json_file:
                     json_file.write(json.dumps(register))
-            case 'csv_windmill':
+            case 'csv_windturbine':
                 df = pd.DataFrame([register])
                 df.to_csv('data/data.csv',index=False)
-            case 'parquet_windmill':
+            case 'parquet_windturbine':
                 df = pd.DataFrame([register])
                 df.to_parquet('data/data.parquet',index=False)
 
